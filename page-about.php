21e2a74480572257f261402b50d9797e1d3d9ef7
majdal01 <145221922+majdal01@users.noreply.github.com>
2024-12-13 10:58:17 +0100

    Tilføjet boks og tekst i about me

--- page-about.php
+++ page-about.php
@@ -24,7 +24,25 @@ get_header();
 
     </header>
 
+    <div class="line"></div>
 
+    <div class="small-gallery-container" style="background-color: <?php echo esc_attr( get_theme_mod( 'amber_small_gallery_container_bg_color', '#ffffff' ) ); ?>;">
+
+        <div class="box-text-wrapper about-wrapper">
+
+            <div class="small-gallery-box" style="background-color: <?php echo esc_attr( get_theme_mod( 'amber_small_gallery_color', '#F0D8BE' ) ); ?>;">
+                <h2 style="color: <?php echo esc_attr( get_theme_mod( 'amber_small_gallery_h2_color', '#000000' ) ); ?>;"><?php echo esc_html( get_theme_mod( 'amber_small_gallery_h2', 'About me' ) ); ?></h2>
+            </div>
+
+            <div class="small-gallery-text">
+                <h2>Lorem Ipsum</h2>
+                <p style="color: <?php echo esc_attr( get_theme_mod( 'amber_small_gallery_p_color', '#000000' ) ); ?>;"><?php echo wp_kses_post( get_theme_mod( 'amber_small_gallery_p', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua <br><br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ) ); ?></p>
+                
+            </div>
+
+        </div>
+
+    </div>
 
     </main><!-- #main -->
 </div><!-- #primary -->
