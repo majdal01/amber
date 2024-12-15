dade1ba74b45ebc9b3932d33502daad53712ba21
majdal01 <145221922+majdal01@users.noreply.github.com>
2024-12-15 19:54:13 +0100

    Ændret default page-php, så den oprettes med layout som resten af temaet. Oprettet muligheden for at kunne tilføje indhold til default page

--- page.php
+++ page.php
@@ -15,24 +15,32 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <header class="page-header">
+        <div class="page-header-img">
+            <?php 
+            $header_image = get_theme_mod( 'amber_page_header_image' );
+            if ( $header_image ) : ?>
+                <img src="<?php echo esc_url( $header_image ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+            <?php else : ?>
+                <img src="<?php echo esc_url( get_template_directory_uri() . '/img/default-page-header.png' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+            <?php endif; ?>
+        </div>
+        <h1 class="page-title"><?php echo esc_html( get_theme_mod( 'amber__page_title', __( 'Customize your page', 'amber' ) ) ); ?></h1>
+    </header>
 
-			get_template_part( 'template-parts/content', 'page' );
+    <div class="line"></div>
+	
+	<?php
+        while ( have_posts() ) : 
+            the_post();
+            the_content(); // This displays all the content from the page editor
+        endwhile;
+    ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
-		endwhile; // End of the loop.
-		?>
+	<div class="greenline"></div>
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
